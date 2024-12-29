f66e1c0bffb9a3af47143b715c76666ef8c002d7
magdaa13 <magdalena.brabec@gmx.net>
2024-12-29 14:31:17 +0100

    neue Patientenklasse; GUI

--- src/Patient.java
+++ src/Patient.java
@@ -14,4 +14,62 @@ public class Patient {
         this.geburtsdatum = geburtsdatum;
         this.diagnose = diagnose;
     }
+
+
+
+
+
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getVorname() {
+        return vorname;
+    }
+
+    public void setVorname(String vorname) {
+        this.vorname = vorname;
+    }
+
+    public String getNachname() {
+        return nachname;
+    }
+
+    public void setNachname(String nachname) {
+        this.nachname = nachname;
+    }
+
+    public String getGeburtsdatum() {
+        return geburtsdatum;
+    }
+
+    public void setGeburtsdatum(String geburtsdatum) {
+        this.geburtsdatum = geburtsdatum;
+    }
+
+    public String getDiagnose() {
+        return diagnose;
+    }
+
+    public void setDiagnose(String diagnose) {
+        this.diagnose = diagnose;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "id=" + id +
+                ", vorname='" + vorname + '\'' +
+                ", nachname='" + nachname + '\'' +
+                ", geburtsdatum='" + geburtsdatum + '\'' +
+                ", diagnose='" + diagnose + '\'' +
+                '}';
+    }
 }
